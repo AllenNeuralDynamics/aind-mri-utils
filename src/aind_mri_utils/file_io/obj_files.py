115c632d9dea2909872fa541eefb848ec0cc152d
Galen Lynch <galen@galenlynch.com>
2023-02-07 12:27:25 -0800

    Add function to extract vertices and faces from pywavefront scenes (#6)
    This extracts vertices and faces into numpy arrays

--- src/aind_mri_utils/file_io/obj_files.py
+++ src/aind_mri_utils/file_io/obj_files.py
@@ -1,6 +1,29 @@
 """Functions for working with obj files"""
 
+import numpy as np
 import pywavefront
+
+
+def get_vertices_and_faces(scene):
+    """
+    Collect vertices and faces for a pywavefront object
+
+    Parameters
+    ==========
+    scene - a Wavefront object made with `collect_materials=True`
+
+    Returns
+    =======
+    vertices - np.ndarray (floating point N x 3) array of N 3d points
+    faces - list[np.ndarray], list of (int M x 3) array of M triangles, where
+            each element corresponds to the index of a vertex in `vertices`.
+            Each list element corresponds to a different mesh.
+    """
+    vertices = np.array(scene.vertices)
+    faces = []
+    for mesh in scene.mesh_list:
+        faces.append(np.array(mesh.faces))
+    return vertices, faces
 
 
 def load_obj_wavefront(filename):
