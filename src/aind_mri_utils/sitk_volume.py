00de470a122b01eddd0ca6c9284c367e67c24c85
yonibrowning <yonib@uw.edu>
2023-02-09 13:25:13 -0800

    Update src/aind_mri_utils/sitk_volume.py
    Co-authored-by: Galen Lynch <galen@galenlynch.com>

--- src/aind_mri_utils/sitk_volume.py
+++ src/aind_mri_utils/sitk_volume.py
@@ -88,7 +88,7 @@ def resample3D(
     transform : SimpleITK Affine Transform, optional
         If no transform is passed, use a identity transform matrix
     output_spacing : (3x1) array, optional
-        If not passed, coppies from image
+        If not passed, copies from image
     output_direction : (9x1) array, optional
         If not passed, coppies from image
     output_origin : (3x1) array, optional
