98ef6f6a39b8aa7a855fe9e9572401f8780afaa8
yonibrowning <yonib@uw.edu>
2024-07-25 10:31:25 -0700

    Chemical shift code now calls correct extensions

--- src/aind_mri_utils/chemical_shift.py
+++ src/aind_mri_utils/chemical_shift.py
@@ -5,7 +5,7 @@ Functions for correcting for chemical shift in MRI images
 import numpy as np
 
 from . import utils as ut
-
+from . import rotations as rot
 
 def compute_chemical_shift(
     image, ppm=(3.7 + 4.1) / 2, mag_freq=599, pixel_bandwidth=500
@@ -54,11 +54,11 @@ def chemical_shift_transform(shift, readout="HF"):
     ValueError: If the readout direction is not recognized.
     """
     if readout == "HF":
-        return ut.make_homogeneous_transform(
+        return rot.make_homogeneous_transform(
             np.eye(3), np.array([0, shift, 0])
         )
     elif readout == "LR":
-        return ut.make_homogeneous_transform(
+        return rot.make_homogeneous_transform(
             np.eye(3), np.array([shift, 0, 0])
         )
     else:
