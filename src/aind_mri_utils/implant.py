f897e8cd40a89b858805513c68cea4c923fb0000
Galen Lynch <galen@galenlynch.com>
2024-12-01 16:32:56 -0800

    Initialize rotation as well as translation of implant

--- src/aind_mri_utils/implant.py
+++ src/aind_mri_utils/implant.py
@@ -1,16 +1,22 @@
 """Module to fit implant rotations to MRI data."""
 
+import warnings
 from concurrent.futures import ProcessPoolExecutor, as_completed
 
 import numpy as np
 from scipy.optimize import fmin
+from scipy.spatial.transform import Rotation
 
 from aind_mri_utils.file_io.slicer_files import get_segmented_labels
 from aind_mri_utils.meshes import (
     distance_to_all_triangles_in_mesh,
     distance_to_closest_point_for_each_triangle_in_mesh,
 )
-from aind_mri_utils.rotations import apply_rotate_translate, combine_angles
+from aind_mri_utils.rotations import (
+    apply_rotate_translate,
+    combine_angles,
+    rotation_matrix_from_vectors,
+)
 from aind_mri_utils.sitk_volume import find_points_equal_to
 
 
@@ -69,7 +75,12 @@ def _implant_cost_fun(T, hole_mesh_dict, hole_seg_dict, run_parallel=True):
     return total_distance
 
 
-def fit_implant_to_mri(hole_seg_dict, hole_mesh_dict, initialization_hole=4):
+def fit_implant_to_mri(
+    hole_seg_dict,
+    hole_mesh_dict,
+    initialization_hole=4,
+    other_init_holes=[3, 9],
+):
     """
     Fits an implant model to MRI data by optimizing the alignment of hole
     segments.
@@ -85,6 +96,9 @@ def fit_implant_to_mri(hole_seg_dict, hole_mesh_dict, initialization_hole=4):
         face has key -1.
     initialization_hole : int, optional
         The hole to use for initialization, by default 4.
+    other_init_holes : int, optional
+        The hole to use for initialization of the rotation, by default [3, 9].
+
 
     Returns
     -------
@@ -92,10 +106,53 @@ def fit_implant_to_mri(hole_seg_dict, hole_mesh_dict, initialization_hole=4):
         The optimized transformation parameters that align the implant model to
         the MRI data.
     """
-    annotation_mean = np.mean(hole_seg_dict[initialization_hole], axis=0)
-    model_mean = np.mean(hole_mesh_dict[initialization_hole].vertices, axis=0)
-    init_offset = model_mean - annotation_mean
-    T = [0, 0, 0, init_offset[0], init_offset[1], init_offset[2]]
+    T = np.zeros(6)
+    initialize_translation = initialization_hole in hole_seg_dict
+    if initialize_translation:
+        annotation_mean = np.mean(hole_seg_dict[initialization_hole], axis=0)
+        model_mean = np.mean(
+            hole_mesh_dict[initialization_hole].vertices, axis=0
+        )
+        init_offset = model_mean - annotation_mean
+        T[3:] = init_offset
+    else:
+        warnings.warn(
+            f"Could not find hole {initialization_hole} in MRI data "
+            "for initialization"
+        )
+
+    # Initial guess of rotation
+    initialize_rotation = initialize_translation and len(other_init_holes) >= 2
+    for other_hole in other_init_holes:
+        initialize_rotation = (
+            initialize_rotation
+            and other_hole in hole_seg_dict
+            and other_hole != initialization_hole
+        )
+        if not initialize_rotation:
+            break
+
+    if initialize_rotation:
+        # Initialize rotation based on pairs of holes
+        #
+        # Find the rotation that aligns the vector between the first two holes
+        # in the annotation to the vector between the first two holes in the
+        # model. Refine with subsequent pairs of holes.
+        R_init = np.eye(3)
+        for other_hole in other_init_holes:
+            other_anno_mean = np.mean(hole_seg_dict[other_hole], axis=0)
+            other_model_mean = np.mean(
+                hole_mesh_dict[other_hole].vertices, axis=0
+            )
+            anno_diff_rotated = R_init @ (other_anno_mean - annotation_mean)
+            R_update = rotation_matrix_from_vectors(
+                anno_diff_rotated,
+                other_model_mean - model_mean,
+            )
+            R_init = R_update @ R_init
+        T[:3] = Rotation.from_matrix(R_init).as_euler("xyz")
+    else:
+        warnings.warn("Could not find holes for rotation initialization")
 
     output = fmin(
         _implant_cost_fun,
