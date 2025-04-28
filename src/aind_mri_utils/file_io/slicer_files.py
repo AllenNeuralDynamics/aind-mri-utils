a3e86c6222dfa10ac74ca2799c6b15faf0a500e2
Galen Lynch <galen@galenlynch.com>
2025-04-28 11:47:56 -0700

    Sanitize inputs to create_slicer_fcsv

--- src/aind_mri_utils/file_io/slicer_files.py
+++ src/aind_mri_utils/file_io/slicer_files.py
@@ -227,6 +227,10 @@ def create_slicer_fcsv(filename, pts_dict, direction="LPS"):
 
         for pt_no, key in enumerate(pts_dict.keys()):
             x, y, z = pts_dict[key]
+            # sanitize the key to remove any problematic characters for CSV
+            # Remove commas, newlines, carriage returns, tabs, quotes, and
+            # leading/trailing whitespace
+            key = re.sub(r'[\s,\'"\\]+', "_", key.strip()).strip("_")
             f.write(
                 "{:d},{:f},{:f},{:f},0,0,0,1,1,1,0,{!s},,vtkMRMLScalarVolumeNode1\n".format(  # noqa: E501
                     pt_no + 1, x, y, z, key
