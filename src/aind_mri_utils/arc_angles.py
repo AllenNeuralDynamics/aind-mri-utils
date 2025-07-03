22fc4a8357896718afe445d0a3358d87de0d0a44
Galen Lynch <galen@galenlynch.com>
2025-07-02 21:23:29 -0700

    Add stereotax angles

--- src/aind_mri_utils/arc_angles.py
+++ src/aind_mri_utils/arc_angles.py
@@ -30,7 +30,8 @@ def calculate_arc_angles(vec, degrees=True, invert_AP=True):
     if np.dot(vec, [0, 0, 1]) < 0:
         vec = -vec
     nv = vec / np.linalg.norm(vec)
-    rx = -np.arcsin(nv[1])  # Our convention is backwards from right hand rule
+    # using trig identity to get the angle from vertical
+    rx = -np.arcsin(nv[1])
     ry = np.arctan2(nv[0], nv[2])
     if degrees:
         rx = np.rad2deg(rx)
@@ -38,6 +39,82 @@ def calculate_arc_angles(vec, degrees=True, invert_AP=True):
     if invert_AP:
         rx = -rx
     return rx, ry
+
+
+def calculate_vector_from_arc_angles(
+    rx, ry, degrees=True, invert_AP=True, invert_rotation=True
+):
+    """
+    Calculate a vector from arc angles.
+
+    Parameters
+    ----------
+    rx : float
+        The angle around the x-axis (anterior-posterior).
+    ry : float
+        The angle around the y-axis (medial-lateral).
+    degrees : bool, optional
+        If True, input angles are in degrees (default is True).
+    invert_AP : bool, optional
+        If True, invert the AP angle to correct for the non-right-handed
+        convention (default is True).
+    invert_rotation : bool, optional
+        If True, invert the rotation angle (default is True).
+
+    Returns
+    -------
+    numpy.ndarray
+        A 3-element vector with ML, AP, and DV components.
+    """
+    if degrees:
+        rx = np.deg2rad(rx)
+        ry = np.deg2rad(ry)
+    if invert_AP:
+        rx = -rx
+
+    vec = np.array(
+        [
+            np.sin(ry) * np.cos(rx),  # ML component
+            -np.sin(rx),  # AP component using trig identity
+            np.cos(ry) * np.cos(rx),  # DV component
+        ]
+    )
+    return vec / np.linalg.norm(vec)
+
+
+def calculate_stereotax_angles(vec, degrees=True):
+    """
+    Calculate the stereotaxic angles for a given vector.
+
+    Parameters
+    ----------
+    vec : array_like
+        A 3-element vector with ML, AP, and DV components. Directions should be
+        in RAS.
+    degrees : bool, optional
+        If True, return angles in degrees (default is True).
+    mount_on_left : bool, optional
+        If True, assume the mount is on the left side (default is True).
+
+    Returns
+    -------
+    tuple of float
+        The calculated stereotaxic angles in degrees. The first element is the
+        angle around the y-axis (AP), and the second element is the angle
+        around the z-axis (DV).  Returns None if the input vector is a zero
+        vector.
+    """
+    if np.linalg.norm(vec) == 0:
+        return None
+    if np.dot(vec, [0, 0, 1]) < 0:
+        vec = -vec
+    nv = vec / np.linalg.norm(vec)
+    ry = np.arccos(nv[2])
+    rz = np.arctan2(nv[1], nv[0]) - np.pi
+    if degrees:
+        ry = np.rad2deg(ry)
+        rz = np.rad2deg(rz)
+    return ry, rz
 
 
 def transform_matrix_from_angles(
