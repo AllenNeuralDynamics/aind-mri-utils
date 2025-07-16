fix: if input covariance matrix has determinant zero, do not mirror

src/aind_mri_utils/reticle_calibrations.py
@@ -110,19 +110,25 @@ def anisotropic_similarity(X, Y, atol_factor=1e-12):
     Xc, Yc = X - Xm, Y - Ym
 
     H = Yc.T @ Xc  # shape (3,3)
+    ndim = H.shape[0]
+    H_det = np.linalg.det(H)
     U, S, Vt = np.linalg.svd(H, full_matrices=False)
     tol = max(H.shape) * np.finfo(S.dtype).eps * S[0]
     rank = np.sum(S > tol)
     # ---- handedness correction ---------------------------------------------
     R = U @ Vt
     d = np.sign(np.linalg.det(R))
-    D = np.eye(3)
+    D = np.eye(ndim)
     if d < 0:
         U[:, -1] *= -1  # Flip last column of U
         R = U @ Vt
         D[-1, -1] = -1  # Flip last diagonal element of D
     # Generate the reflection correction matrix F
-    F = Vt.T @ D @ Vt
+    # If Hdet is close to zero, use the identity matrix
+    if np.isclose(H_det, 0):
+        F = np.eye(ndim)
+    else:
+        F = Vt.T @ D @ Vt
 
     # Compute scaling factors robustly
     X_rot = (Xc @ F) @ R.T
@@ -133,7 +139,7 @@ def anisotropic_similarity(X, Y, atol_factor=1e-12):
     denom = (X_rot * X_rot).sum(0)
     deg = denom < atol  # boolean mask
 
-    scale = np.ones(3)  # default 1
+    scale = np.ones(ndim)  # default 1
     scale[~deg] = num[~deg] / denom[~deg]
 
     # ---- translation --------------------------------------------------------